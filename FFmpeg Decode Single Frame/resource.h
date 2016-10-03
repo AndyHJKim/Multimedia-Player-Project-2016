1d1239591511e09f5a2492962d80e29b36f0f287
AndyHJKim <andyjun1227@gmail.com>
2016-10-03 22:48:56 +0900

    Beginning

---
+++ FFmpeg Decode Single Frame/resource.h
@@ -0,0 +1,12 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by player.rc
+
+#define IDD_MFPLAYBACK_DIALOG           102
+#define IDM_EXIT                        105
+#define IDC_MFPLAYBACK                  109
+#define IDD_OPENURL                     129
+#define IDC_EDIT_URL                    1000
+#define ID_FILE_OPENFILE                32771
+#define ID_FILE_OPENURL                 32772
+#define IDC_STATIC                      -1
